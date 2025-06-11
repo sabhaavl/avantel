556949c0441d18c6312cea26eb901c4742e03ecd
Sabha <sabha@simply.science>
2025-06-11 13:20:16 +0530

    110625-4

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@
 // exports.PASSWORD = "tgbhbzvanbntmhbe";
 
 exports.EMAIL = "sabha@simply.science";
-exports.PASSWORD = "Simply@2024"; 
+exports.PASSWORD = "qzwz kofn oalc hyie"; 
 
 //exports.EMAIL = "contact.us@avantel.in";
 //exports.PASSWORD = "Dell#123";
