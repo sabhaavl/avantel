2c65f164aa794125d8012a177f3a65d46c4609e8
Sabha <sabha@simply.science>
2025-06-17 15:52:06 +0530

    avl-170625-6

--- conf.js
+++ conf.js
@@ -1,8 +1,8 @@
 // exports.EMAIL = "info@imedsglobal.com";
 // exports.PASSWORD = "tgbhbzvanbntmhbe";
 
-//exports.EMAIL = "sabha@simply.science";
-//exports.PASSWORD = "qzwz kofn oalc hyie"; 
+exports.EMAIL = "sabha@simply.science";
+exports.PASSWORD = "qzwz kofn oalc hyie"; 
 //exports.PASSWORD =
-exports.EMAIL = "contact.us@avantel.in";
-exports.PASSWORD = "cnfpgkkrvmdnfcnh";
+//exports.EMAIL = "contact.us@avantel.in";
+//exports.PASSWORD = "cnfpgkkrvmdnfcnh";
