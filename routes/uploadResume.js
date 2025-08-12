4ff3a58e1f2f005bdee11f10707c45fd9b5e2179
Sabha <sabha@simply.science>
2025-08-12 18:32:35 +0530

    avl-120825-9

--- routes/uploadResume.js
+++ routes/uploadResume.js
@@ -13,7 +13,7 @@ let transporter = nodemailer.createTransport({
 module.exports = (req, res) => {
     // if (!req.file) {
     //     return res.status(400).send('No file uploaded.');
-    // }
+    // } 
    // let sendTo = 'hiring.avl@avantel.in';
    let sendTo = 'sabha@simply.science';
     //let ccTo = 'sabha@simply.science';
