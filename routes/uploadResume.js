715a85d86c87627b9fec5139853fd21f5ac26a98
Sabha <sabha@simply.science>
2025-12-18 10:29:16 +0530

    avl-181225-2

--- routes/uploadResume.js
+++ routes/uploadResume.js
@@ -17,8 +17,8 @@ module.exports = (req, res) => {
     // if (!req.file) {
     //     return res.status(400).send('No file uploaded.');
     // }
-     let sendTo = 'hiring.avl@avantel.in';
-   // let sendTo = 'sabha@simply.science'
+   //  let sendTo = 'hiring.avl@avantel.in';
+    let sendTo = 'sabha@simply.science'
     //let ccTo = 'sabha@simply.science';
     // Email options
     let mailOptions = {
